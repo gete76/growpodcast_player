26aae32efb1db333c3a612ef5036b79462fe53c2
Greg Traub <greg.traub@gmail.com>
2015-04-07 00:02:42 -0400

    more work

--- public/player/js/main.js
+++ public/player/js/main.js
@@ -1,3 +1,18 @@
 $ = jQuery = require('jquery');
-var React = require('react');
-var wavesurfer = require('wavesurfer');
+require('./lib/flat.audio');
+require('./lib/jquery.jplayer');
+
+//var React = require('react');
+/*var WS = require('wavesurfer.js');
+
+var wavesurfer = Object.create(WS);
+
+wavesurfer.init({
+    container: '#player',
+    waveColor: 'white',
+    progressColor: 'purple',
+    pixelRatio: 1,
+    height:30
+});
+
+wavesurfer.load('https://s3.amazonaws.com/growradio/no_filler/no_filler_2014120400.mp3');*/
