c76119113d8d87bfd7a467a24ef87c1ffa749f0c
Greg Traub <greg.traub@gmail.com>
2015-04-08 21:58:06 -0400

    adding jplayer with flat audio

--- public/player/js/main.js
+++ public/player/js/main.js
@@ -2,17 +2,24 @@ $ = jQuery = require('jquery');
 require('./lib/flat.audio');
 require('./lib/jquery.jplayer');
 
-//var React = require('react');
-/*var WS = require('wavesurfer.js');
-
-var wavesurfer = Object.create(WS);
-
-wavesurfer.init({
-    container: '#player',
-    waveColor: 'white',
-    progressColor: 'purple',
-    pixelRatio: 1,
-    height:30
+$(document).ready(function(){
+  $('#player').jPlayer({
+    ready: function () {
+      $(this).jPlayer("setMedia", {
+        title: "No Filler - Dec 04 2014",
+        mp3: 'https://s3.amazonaws.com/growradio/no_filler/no_filler_2014120400.mp3'
+      });
+    },
+    cssSelectorAncestor: "#jp_container_1",
+    swfPath: "/js",
+    supplied: "mp3",
+    useStateClassSkin: true,
+    autoBlur: false,
+    smoothPlayBar: true,
+    keyEnabled: true,
+    remainingDuration: true,
+    toggleDuration: true
+  });
 });
 
-wavesurfer.load('https://s3.amazonaws.com/growradio/no_filler/no_filler_2014120400.mp3');*/
+//wavesurfer.load('https://s3.amazonaws.com/growradio/no_filler/no_filler_2014120400.mp3');
